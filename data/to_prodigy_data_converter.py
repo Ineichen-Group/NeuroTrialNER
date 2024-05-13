c58cbe0c9911e8a69ef5f4d88a51cda542317bdb
Simona D <donevasimona@gmail.com>
2024-05-13 12:25:26 +0200

    processed new samples with minority classes

--- data/to_prodigy_data_converter.py
+++ data/to_prodigy_data_converter.py
@@ -13,6 +13,7 @@ def convert_text_to_json_official_random_text(row):
     else:
         return json.dumps({'nct_id': str(row['nct_id']), 'source': "BriefSummary", 'text': row['description']})
 
+
 # TODO: make more generic, currently very fixed for ClinicalTrials data!
 def convert_text_to_json_official_combined_text(row):
     if not isinstance(row['official_title'], str) and (math.isnan(row['official_title'])):
@@ -32,32 +33,56 @@ def save_text_json_to_csv(df, file_path_name):
 if __name__ == '__main__':
     today = date.today()
     today_formatted = today.strftime("%Y%m%d")
+    process_second_batch = False
+    process_first_batch = False
+    process_non_drug_batch = False
+    process_minority_classes = True
+
     file_name_batch_1 = "data_aact_sample/random_sample_neurological_with_summaries_202306151741.csv"
     file_name_batch_2 = "data_aact_sample/aact_neuro_samples_second_batch_202309071141.csv"
+    file_name_non_drug = "data_aact_sample/aact_neuro_samples_20240422_non_drug_biological_dietary_150.csv"
+    file_name_minority_classes = "data_aact_sample/aact_neuro_samples__20240513_minority_classes.csv"
 
-    df1 = pd.read_csv(file_name_batch_1)
+    if process_minority_classes:
+        df_clin_trials_sample = pd.read_csv(file_name_minority_classes)
+        df_clin_trials_sample['text_json'] = df_clin_trials_sample.apply(convert_text_to_json_official_combined_text,
+                                                                         axis=1)
 
-    # Load the second CSV file into another DataFrame
-    df2 = pd.read_csv(file_name_batch_2)
+        save_text_json_to_csv(df_clin_trials_sample, "./data_for_prodigy"
+                                                     "/ct_random_neurological_minority_classes_{}_{}.jsonl".format(len(df_clin_trials_sample), today_formatted))
 
-    # Convert the 'nct_id' columns to sets and check for an overlap
-    set1 = set(df1['nct_id'])
-    set2 = set(df2['nct_id'])
+    if process_non_drug_batch:
+        df_clin_trials_sample = pd.read_csv(file_name_non_drug)
+        df_clin_trials_sample['text_json'] = df_clin_trials_sample.apply(convert_text_to_json_official_combined_text,
+                                                                         axis=1)
 
-    overlap = not set1.isdisjoint(set2)
+        save_text_json_to_csv(df_clin_trials_sample, "./data_for_prodigy"
+                                                     "/ct_random_neurological_non_drug_{}_{}.jsonl".format(150, today_formatted))
 
-    if overlap:
-        print("The CSV files have an overlap in the 'nct_id' column.")
-    else:
-        print("The CSV files do not have an overlap in the 'nct_id' column.")
+    if process_second_batch:
+        df1 = pd.read_csv(file_name_batch_1)
 
-    df_clin_trials_sample = pd.read_csv(file_name_batch_2)
-    df_clin_trials_sample['text_json'] = df_clin_trials_sample.apply(convert_text_to_json_official_combined_text,
-                                                                     axis=1)
-    save_text_json_to_csv(df_clin_trials_sample, "./data_for_prodigy"
-                                  "/ct_random_neurological_{}_{}_second_batch.jsonl".format(500, today_formatted))
+        # Load the second CSV file into another DataFrame
+        df2 = pd.read_csv(file_name_batch_2)
 
-    process_first_batch = True
+        # Convert the 'nct_id' columns to sets and check for an overlap
+        set1 = set(df1['nct_id'])
+        set2 = set(df2['nct_id'])
+
+        overlap = not set1.isdisjoint(set2)
+
+        if overlap:
+            print("The CSV files have an overlap in the 'nct_id' column.")
+        else:
+            print("The CSV files do not have an overlap in the 'nct_id' column.")
+
+        df_clin_trials_sample = pd.read_csv(file_name_batch_2)
+        df_clin_trials_sample['text_json'] = df_clin_trials_sample.apply(convert_text_to_json_official_combined_text,
+                                                                         axis=1)
+        save_text_json_to_csv(df_clin_trials_sample, "./data_for_prodigy"
+                                                     "/ct_random_neurological_{}_{}_second_batch.jsonl".format(500,
+                                                                                                               today_formatted))
+
     # First annotations batch done in two-rounds, i.e. we first annotated 100 samples and refined the annotation guidelines, then another 400 samples were annotated
     if process_first_batch:
         df_clin_trials_sample = pd.read_csv(file_name_batch_1)
