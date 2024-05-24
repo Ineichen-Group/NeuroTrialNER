c5df3992c815f00bd2fd1138dfdb7ed3e13b5d4f
Simona Doneva <donevasimona@gmai.com>
2024-05-24 11:28:42 +0200

    added code to balance intervention entities distribution during tran,dev,test split

--- data/convert_to_bio_and_generate_dataset_split.py
+++ data/convert_to_bio_and_generate_dataset_split.py
@@ -1,11 +1,12 @@
 import json
 import random
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
 
-
-def read_json_split_data_to_csv(input_json_filename,
-                                additional_annotation_format_file="ct_neuro_final_target_annotated_ds.csv",
-                                output_data_path='corpus/test_annotations_for_guidlines/'):
+def read_json_split_save_data(input_json_filename,
+                              additional_annotation_format_file="ct_neuro_final_target_annotated_ds.csv",
+                              output_data_path='corpus/test_annotations_for_guidlines/'):
     # Set a random seed for reproducibility
     random.seed(42)
 
@@ -106,7 +107,7 @@ def convert_annotations_to_bio_format(row):
     }
 
 
-def process_file_bert_to_bio_format(input_jsonl_file_prodigy_format, output_jsonl_file_bio_format):
+def process_file_and_save_bert_to_bio_format(input_jsonl_file_prodigy_format, output_jsonl_file_bio_format):
     print("Processing file: ", input_jsonl_file_prodigy_format)
     # Load the data from the file
     with open(input_jsonl_file_prodigy_format, 'r') as file:
@@ -117,9 +118,11 @@ def process_file_bert_to_bio_format(input_jsonl_file_prodigy_format, output_json
     data_size = len(converted_data)
 
     # Save the converted data to a new file
-    with open(output_jsonl_file_bio_format, 'w') as file:
+    output_jsonl_file_bio_format_formatted = output_jsonl_file_bio_format + f"_{data_size}.jsonl"
+    with open(output_jsonl_file_bio_format_formatted, 'w') as file:
         for row in converted_data:
             file.write(json.dumps(row) + '\n')
+
         # Calculate the maximum, minimum, and average number of tokens
         num_tokens = [len(row["tokens"]) for row in converted_data]
         max_tokens = max(num_tokens)
@@ -129,18 +132,119 @@ def process_file_bert_to_bio_format(input_jsonl_file_prodigy_format, output_json
         print("Maximum number of tokens:", max_tokens)
         print("Minimum number of tokens:", min_tokens)
         print("Average number of tokens:", avg_tokens)
+    return output_jsonl_file_bio_format_formatted
+
+def custom_split_stratified(input_json_filename, output_data_path, additional_annotation_format_file, entities, train_size=0.8, valid_size=0.1):
+    print("Performing stratified split.")
+    # Set a random seed for reproducibility
+    random.seed(42)
+
+    # Load the data from the file
+    with open(input_json_filename, 'r') as file:
+        data = [json.loads(row) for row in file]
+
+    # Shuffle the data randomly
+    random.shuffle(data)
+
+    # Convert the data to DataFrame objects
+    df = pd.DataFrame(data)
+    # load the dataset that is not bio format
+    additional_format = pd.read_csv(additional_annotation_format_file)[
+        ["nct_id", "text", "ner_manual_ct_target"]]
+
+    # Join and keep only the rows from each split
+    df = pd.merge(df, additional_format, left_on='id', right_on='nct_id', how='left')
+
+    test_size = 1.0 - train_size - valid_size
+
+    train_list = []
+    valid_list = []
+    test_list = []
+
+    for entity in entities:
+        entity_df = df[df['ner_manual_ct_target'].str.contains(entity, na=False)]
+        print(entity, len(entity_df))
+
+        # Proceed with traditional splitting
+        train, val_test = train_test_split(entity_df, train_size=train_size, test_size=test_size + valid_size,
+                                            random_state=42)
+        valid, test = train_test_split(val_test, train_size = valid_size / (valid_size + test_size), random_state=42)
+
+        train_list.append(train)
+        valid_list.append(valid)
+        test_list.append(test)
+        print(f"Train size {len(train)}, valid size {len(valid)}, test size {len(test)}")
+        # Remove processed rows from the main DataFrame
+        df = df.drop(entity_df.index)
+
+    if not df.empty:
+        train, val_test = train_test_split(df, train_size=train_size, test_size=test_size+valid_size,
+                                            random_state=42)
+        valid, test = train_test_split(val_test, train_size=valid_size / (valid_size + test_size), random_state=42)
+
+        train_list.append(train)
+        valid_list.append(valid)
+        test_list.append(test)
+
+    train_final = pd.concat(train_list, ignore_index=True)
+    valid_final = pd.concat(valid_list, ignore_index=True)
+    test_final = pd.concat(test_list, ignore_index=True)
+
+    if len(valid_final) != len(test_final):
+        size_difference = abs(len(valid_final) - len(test_final))
+        transfer_count = size_difference // 2  # Transfer half of the difference
+
+        if len(valid_final) < len(test_final):
+            # Remove the last row from the larger DataFrame and append it to the smaller DataFrame
+            for _ in range(transfer_count):
+                removed_row = test_final.iloc[[-1]]  # Get the last row as a DataFrame
+                valid_final = pd.concat([valid_final, removed_row], ignore_index=True)
+                test_final = test_final.drop(test_final.index[-1])
+        else:
+            for _ in range(transfer_count):
+                removed_row = valid_final.iloc[[-1]]  # Get the last row as a DataFrame
+                test_final = pd.concat([test_final, removed_row], ignore_index=True)
+                valid_final = valid_final.drop(valid_final.index[-1])
+
+    # Shuffle indices
+    train_indices = np.random.permutation(train_final.index)
+    valid_indices = np.random.permutation(valid_final.index)
+    test_indices = np.random.permutation(test_final.index)
+
+    # Reorder data frames based on shuffled indices
+    train_final = train_final.loc[train_indices].reset_index(drop=True)
+    valid_final = valid_final.loc[valid_indices].reset_index(drop=True)
+    test_final = test_final.loc[test_indices].reset_index(drop=True)
+
+    print(f"Train size {len(train_final)}, valid size {len(valid_final)}, test size {len(test_final)}")
+
+    train_final.to_csv(output_data_path + 'ct_neuro_train_merged_{}.csv'.format(len(train_final)), index=False)
+    valid_final.to_csv(output_data_path + 'ct_neuro_dev_merged_{}.csv'.format(len(valid_final)), index=False)
+    test_final.to_csv(output_data_path + 'ct_neuro_test_merged_{}.csv'.format(len(test_final)), index=False)
+
+    return train_final, valid_final, test_final
 
 
 if __name__ == '__main__':
     # used for the evaluation -> prodigy tokenizer is not the same as the BERT tokenizer -> when comparing the target annotation array from prodigy it is not the same to BERT
     # simulating how BERT will tokenize the sentence -> adjust the prodigy annotations to make it comparable
     files_directory = "./annotated_data/final_combined/"
-    output_json_with_bio_format = files_directory + 'ct_neuro_final_target_annotated_ds_bio_format.jsonl'
-    process_file_bert_to_bio_format(
-        files_directory + '/ct_neuro_final_target_annotated_ds_combined_rounds.jsonl',
+    file_suffix = "_incl_round_3"
+    input_jsonl_file_prodigy_format = files_directory + f'/ct_neuro_final_target_annotated_ds_combined_rounds{file_suffix}.jsonl'
+
+    output_json_with_bio_format = files_directory + 'ct_neuro_final_target_annotated_ds_bio_format'
+
+    output_jsonl_file_bio_format_formatted = process_file_and_save_bert_to_bio_format(
+        input_jsonl_file_prodigy_format,
         output_jsonl_file_bio_format=output_json_with_bio_format)
 
-    # SPLIT INTO TRAIN, DEV, TEST
-    read_json_split_data_to_csv(output_json_with_bio_format,
-                                additional_annotation_format_file=files_directory+"ct_neuro_final_target_annotated_ds_combined_rounds.csv",
-                                output_data_path='./annotated_data/data_splits/')
+    output_data_splits_path = './annotated_data/data_splits/'
+    # SPLIT RANDOMLY INTO TRAIN, DEV, TEST
+    file_with_non_bio_annotations = files_directory + "ct_neuro_final_target_annotated_ds_combined_rounds_incl_round_3.csv"
+    read_json_split_save_data(output_jsonl_file_bio_format_formatted,
+                              additional_annotation_format_file=file_with_non_bio_annotations,
+                              output_data_path=output_data_splits_path)
+
+    # SPLIT WITH BALANCED ENTITIES INTO TRAIN, DEV, TEST
+    entities_intervention_to_balance = ['RADIOTHERAPY', 'SURGICAL', 'BEHAVIOURAL', 'PHYSICAL', 'DRUG', 'OTHER']
+    custom_split_stratified(output_jsonl_file_bio_format_formatted, output_data_splits_path + "stratified_entities/", file_with_non_bio_annotations, entities_intervention_to_balance, train_size=0.8, valid_size=0.1)
