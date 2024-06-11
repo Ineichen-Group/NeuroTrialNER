d0905cdc3724a21b03e5cea73a9966f7e56fd8f5
Simona Doneva <donevasimona@gmai.com>
2024-06-11 16:59:26 +0200

    updated to get IAA model - target annotations

--- models/evaluate.py
+++ models/evaluate.py
@@ -49,28 +49,30 @@ if __name__ == '__main__':
 
     target_dataset = "clintrials"
     run_linkbert = False
-    run_biobert = False
+    run_biobert = True
     run_regex = False
-    run_basebert = True
+    run_basebert = False
 
     train_entities_distr_df = pd.read_csv("../data/annotated_data/corpus_stats/clintrials_train_entities_stats.csv")
     train_entities_distr_dict = train_entities_distr_df.set_index('entity_token').to_dict(orient='index')
+    train_data_file = "../data/annotated_data/data_splits/stratified_entities/ct_neuro_train_data_787.json"
 
-    annotated_files_path_prefix = "./predictions/"
+    bert_annotated_files_path_prefix = "./predictions/rebuttal/bert/"
     output_evaluation_path_prefix = "./evaluations/"
+
     if run_basebert:
         print("Eval BERT-base model...")
         hugging_face_model_name = "bert-base-uncased"
         model_name_str = "bert-base-uncased"
-        annotated_data_path = annotated_files_path_prefix + "ct_neuro_test_annotated_{}_{}.csv".format(model_name_str, "20240131")
-        annotated_data_path_bio = annotated_files_path_prefix + "ct_neuro_test_annotated_{}_BIO_{}.csv".format(model_name_str, "20240131")
+        annotated_data_path = bert_annotated_files_path_prefix + "ct_neuro_test_annotated_{}_{}.csv".format(model_name_str, "20240131")
+        annotated_data_path_bio = bert_annotated_files_path_prefix + "ct_neuro_test_annotated_{}_BIO_{}.csv".format(model_name_str, "20240131")
         df = pd.read_csv(annotated_data_path)
         evaluator = ModelEvaluator(target_dataset, df, source_id_col_name="nct_id", source_sent_col_name='text',
                                    train_entities_distr_dict=train_entities_distr_dict,
                                    confidence=0.95, output_file_path=output_evaluation_path_prefix)
 
         print("*** Evaluating BIO format - ENTITY level***")
-        print(evaluator.evaluate_bert_bio(annotated_data_path_bio, "../data/annotated_data/data_splits/ct_neuro_train_data_713.json",
+        print(evaluator.evaluate_bert_bio(annotated_data_path_bio, train_data_file,
                                           "ner_tags", return_format="all", target_labels_column="labels",
                                           predicted_labels_column=f"predictions_{model_name_str}"))
         print("*** Evaluating exact match format ***")
@@ -80,15 +82,15 @@ if __name__ == '__main__':
         print("Eval LinkBERT model...")
         hugging_face_model_name = "michiyasunaga/BioLinkBERT-base"
         model_name_str = hugging_face_model_name.split("/")[1]
-        annotated_data_path = annotated_files_path_prefix + "ct_neuro_test_annotated_{}_{}.csv".format(model_name_str, "20230916")
-        annotated_data_path_bio = annotated_files_path_prefix + "ct_neuro_test_annotated_{}_BIO_{}.csv".format(model_name_str, "20230916")
+        annotated_data_path = bert_annotated_files_path_prefix + "ct_neuro_test_annotated_{}_{}.csv".format(model_name_str, "20230916")
+        annotated_data_path_bio = bert_annotated_files_path_prefix + "ct_neuro_test_annotated_{}_BIO_{}.csv".format(model_name_str, "20230916")
         df = pd.read_csv(annotated_data_path)
         evaluator = ModelEvaluator(target_dataset, df, source_id_col_name="nct_id", source_sent_col_name='text',
                                    train_entities_distr_dict=train_entities_distr_dict,
                                    confidence=0.95, output_file_path=output_evaluation_path_prefix)
 
         print("*** Evaluating BIO format - ENTITY level***")
-        print(evaluator.evaluate_bert_bio(annotated_data_path_bio, "../data/annotated_data/data_splits/ct_neuro_train_data_713.json",
+        print(evaluator.evaluate_bert_bio(annotated_data_path_bio, train_data_file,
                                           "ner_tags", return_format="all", target_labels_column="labels",
                                           predicted_labels_column=f"predictions_{model_name_str}"))
         print("*** Evaluating exact match format ***")
@@ -98,17 +100,17 @@ if __name__ == '__main__':
         print("Eval BioBERT model...")
         hugging_face_model_name = "dmis-lab/biobert-v1.1"
         model_name_str = hugging_face_model_name.split("/")[1]
-        annotated_data_path = annotated_files_path_prefix + "ct_neuro_test_annotated_{}_{}.csv".format(model_name_str,
-                                                                                                       "20230916")
-        annotated_data_path_bio = annotated_files_path_prefix + "ct_neuro_test_annotated_{}_BIO_{}.csv".format(
-            model_name_str, "20230922")
+        annotated_data_path = bert_annotated_files_path_prefix + "ct_neuro_test_annotated_{}_{}.csv".format(model_name_str,
+                                                                                                       "20240529")
+        annotated_data_path_bio = bert_annotated_files_path_prefix + "ct_neuro_test_annotated_{}_BIO_{}.csv".format(
+            model_name_str, "20240529")
         df = pd.read_csv(annotated_data_path)
         evaluator = ModelEvaluator(target_dataset, df, source_id_col_name="nct_id", source_sent_col_name='text',
                                    train_entities_distr_dict=train_entities_distr_dict,
                                    confidence=0.95, output_file_path=output_evaluation_path_prefix)
         print("*** Evaluating BIO format - ENTITY level***")
 
-        print(evaluator.evaluate_bert_bio(annotated_data_path_bio, "../data/annotated_data/data_splits/ct_neuro_train_data_713.json",
+        print(evaluator.evaluate_bert_bio(annotated_data_path_bio, train_data_file,
                                           "ner_tags", return_format="all", target_labels_column="labels",
                                           predicted_labels_column=f"predictions_{model_name_str}"))
         print("*** Evaluating exact match format ***")
